24916e1c85749bb42c5870a0675e562498e9c1cd
nikhil199710 <71897566+nikhil199710@users.noreply.github.com>
2020-11-29 18:23:35 +0530

    Update UC4_ViewData.sql

--- UC4_ViewData.sql
+++ UC4_ViewData.sql
@@ -1,4 +1,4 @@
-/* Reading the entire data from the employee payroll table
- Retreiving all the data*/
- use payroll_services;
- select * from employee_payroll;
+
+use payroll_service;
+/*Retreiving data from table*/
+select * from employee_payroll;
