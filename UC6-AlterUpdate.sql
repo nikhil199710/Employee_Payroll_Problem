0bf167db542dcc121158b633a90063e52cb7030b
nikhil199710 <71897566+nikhil199710@users.noreply.github.com>
2020-11-29 18:25:32 +0530

    Update UC6-AlterUpdate.sql

--- UC6-AlterUpdate.sql
+++ UC6-AlterUpdate.sql
@@ -1,12 +1,4 @@
-use payroll_services;
-select * from employee_payroll;
-/* Adding the gender attribute to the employee payroll table*/
-alter table employee_payroll add gender char(1);
-/* Updating to add the value of gender in the employee payroll table*/
-update employee_payroll set gender = 'F' where EMPNAME = 'Terissa';
-update employee_payroll set gender = 'M' where EMPNAME = 'nikhil';
-update employee_payroll set gender = 'F' where EMPNAME = 'Aliatta';
-update employee_payroll set gender = 'M' where EMPNAME = 'prince';
-update employee_payroll set gender = 'M' where EMPNAME = 'Aseeb';
-update employee_payroll set gender = 'M' where EMPNAME = 'akhil';
-update employee_payroll set gender = 'M' where EMPNAME = 'Manohar';
+/*Add new column to table of gender*/
+alter table employee_payroll add Gender char(1);
+/*Update table*/
+update employee_payroll set Gender='M' where EmpName ='Nikhil' or EmpName ='Morgan';
