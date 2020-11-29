8fb343d613daf34b35c5aeb2d40b8e3060859d7c
nikhil199710 <71897566+nikhil199710@users.noreply.github.com>
2020-11-29 18:22:41 +0530

    Update UC3_Insert data.sql

--- UC3_Insert data.sql
+++ UC3_Insert data.sql
@@ -1,15 +1,14 @@
-  /*UC3-insert data*/
-use payroll_services;
+/* UC 3 :
+Insert data into the table */
+use payroll_service;
+/* Creating a table */
 create table employee_payroll
-( EMPID int not null identity(1000,1) primary key,
-EMPNAME varchar(150) not null,
-SALARY float not null,
-STARTDATE date not null);
-/* Reading the entire data from the employee payroll table*/
-select * from employee_payroll;
-/* Inserting the data from the employee payroll table excluding the ID as it is default ad auto-increment*/
-insert into employee_payroll(EMPNAME, SALARY, STARTDATE)
-values ('nikhil', 30000, '2018-01-03'),
-('Terissa', 25000, '2019-06-08'), ('Aliatta', 35000, '2019-07-08'),
-('prince', 40000, '2018-04-06'), ('Aseeb', 33000, '2016-06-08'),
-('akhil', 45000, '2014-06-08'), ('Manohar', 55000, '2017-06-08');
+(EmpID int not null identity(1,1) primary key,
+EmpName varchar(150) not null,
+Salary float not null,
+StartDate date not null
+);
+/* Inserting data into table */
+insert into employee_payroll(EmpName,Salary,StartDate) values
+('Nikhil',72000,'2020-09-18'),
+('Morgan',180000,'2020-10-26');
