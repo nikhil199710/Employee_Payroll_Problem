868c222904f75aebe2e792a39afbf0c40271c8c3
nikhil199710 <71897566+nikhil199710@users.noreply.github.com>
2020-11-29 18:33:24 +0530

    Update UC11_ImplementER_into_payroll.sql

--- UC11_ImplementER_into_payroll.sql
+++ UC11_ImplementER_into_payroll.sql
@@ -1,42 +1,47 @@
-/* Invoking the payroll services database */
-use payroll_services;
-/* Retrieving all the records from the employee payroll table */
-select * from employee_payroll;
-/* Creating the payroll table to handle the payroll computation
-of employee */
+/*UC 11:
+ER diagram*/
+use payroll_service;
+
+/*Company table and adding data to it*/
+create table company(CompanyId int not null primary key, CompanyName varchar(150) not null);
+insert into company values(500,'Flipkart');
+insert into company values(600,'Myntra');
+select * from company;
+
+/*Employee table and adding data to it*/
+create table employee 
+(EmpId int not null identity(1,1) primary key,
+EmpName varchar(150) not null,
+Gender char(1) not null,
+PhoneNo bigint not null,
+Address varchar(500) not null,
+StartDate date not null,
+CompanyId int not null foreign key references company(CompanyId));
+insert into employee values ('Nikhil','M',7206183244,'Agra','2020-09-18',500);
+insert into employee values ('neha','F',9457127771,'New York','1996-10-24',500);
+insert into employee values ('akhil','M',9410884202,'Canada','2003-12-26',500);
+insert into employee values ('akki','M',935054985,'Tennessee','1964-08-25',600);
+insert into employee values ('sneha','F',8521479630,'Georgia','1985-10-19',600);
+
+/*Payroll table and adding data to it*/ 
 create table payroll
-( basic_pay int not null primary key,
-deductions float not null ,
-taxable_pay float not null,
-income_tax float not null, 
-net_pay float not null);
-/* Creating employee department to handle the multi valued
-data integrity issue for the department attribute */
-create table employee_department
-( employee_id int not null primary key,
-department varchar(50) not null ,
-basic_pay int not null);
-/* Removing the data Redundancy for now */
-delete from employee_payroll where EMPID = 1007;
-/* Retrieving all the records from the employee department table */
-select * from employee_department;
-/* Adding the data to the employee department table */
-insert into employee_department
-values (1000, 'Accounts', 45000), (1001, 'Sales', 25000),
-(1001, 'Marketing', 20000), (1002, 'Manager', 70000),
-(1003, 'Developer', 50000), (1004, 'Architect', 30000),
-(1005, 'CEO', 90000), (1006, 'CTO', 80000);
-/* Retrieving all the records from the payroll table */
-select * from payroll;
-/* Adding pnly the simple attribute data to the payroll table */
-insert into payroll(basic_pay, deductions, income_tax, taxable_pay, net_pay)
-values (20000, 500, 1000, 19000, 18500), (25000, 700, 1000, 24000, 23300),
-(30000, 1000, 1000, 29000, 28000), (45000, 2000, 1500, 43500, 41500),
-(50000, 2500, 2000, 48000, 45500), (70000, 4000, 7000, 63000, 59000),
-(80000, 6000, 8000, 72000, 66000), (90000, 7000, 9000, 81000, 74000);
-/*Denoting the Sum Of the salary, Count of employee, Minimum Salary And Maximum salary
-from the Payroll Services Databases grouping by Gender */
-select emp.gender as 'Gender', SUM(dept.basic_pay) as 'Sum Of Salary', MIN(dept.basic_pay) as 'Minimum Salary', 
-MAX(dept.basic_pay) as 'Maximum Salary', COUNT(distinct(emp.EMPNAME)) as 'Number Of Employee',
-AVG(dept.basic_pay) as 'Average Salary' from employee_payroll emp, payroll pay, employee_department dept
-where emp.EMPID = dept.employee_id and dept.basic_pay = pay.basic_pay group by emp.gender;
+(EmpId int not null primary key references employee(EmpId), BasicPay decimal,Deductions decimal,TaxablePay decimal,IncomeTax decimal,NetPay decimal);
+insert into payroll values (1,72000,3000,69000,1200,85200);
+insert into payroll values (2,160000,15000,145000,2400,74100);
+insert into payroll values (3,3200000,20000,3180000,3600,96300);
+insert into payroll values (4,450000,50000,400000,4800,85400);
+insert into payroll values (5,5600000,60000,5540000,6000,85600);
+
+/*Department table and adding data to it*/
+create table department (DeptId int not null primary key, DeptName varchar(100) not null);
+insert into department values (5,'Sales');
+insert into department values (10,'Marketing');
+
+/*EmpDepartment table and adding data to it*/
+create table empDepartment (EmpId int not null foreign key references employee(EmpId), DeptId int not null foreign key references department(DeptId));
+insert into empDepartment values (1,5);
+insert into empDepartment values (2,5);
+insert into empDepartment values (2,10);
+insert into empDepartment values (3,5);
+insert into empDepartment values (4,10);
+insert into empDepartment values (5,10);
